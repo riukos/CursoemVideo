414e263f0554166329c6ad51666396e7414335b8
FelipeAndrade <riiuuku@gmail.com>
2023-01-04 12:56:20 -0300

    Resolvi o ex104 python

---
+++ pythonProject1/desafio104.py
@@ -0,0 +1,18 @@
+def leiaInt(msg):
+    ok = False
+    valor = 0
+    while True:
+        n = str(input(msg))
+        if n.isnumeric():
+            valor = int(n)
+            ok = True
+        else:
+            print('\033[0;31mErro! Digite um numero inteiro válido. \033[m')
+        if ok:
+            break
+    return valor
+
+
+#Programa pirncial
+n = leiaInt('Digite um número: ')
+print(f'Você acabou de digitar o numero: {n}')
