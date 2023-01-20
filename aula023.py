19381578e28850996971432a8adb1ff1dbc0bef1
FelipeAndrade <riiuuku@gmail.com>
2023-01-20 20:42:02 -0300

    Aula023 assistida com sucesso
    aprendi a tratar erro com try e except

---
+++ aula023.py
@@ -0,0 +1,15 @@
+try:
+    a = int(input('Numerador: '))
+    b = int(input('Denominador: '))
+    r = a / b
+except (ValueError, TypeError):
+    print('Tivemos um problema com os tipo de dados que você digitou!')
+except ZeroDivisionError:
+    print('Não é possivel dividir um numero por zero!')
+except KeyboardInterrupt:
+    print('O usuario decidiu não informa os dados')
+else:
+    print(f'O resultado é {r:.1f}')
+finally:
+    print('Volte sempre! Muito Obrigado!')
+    
